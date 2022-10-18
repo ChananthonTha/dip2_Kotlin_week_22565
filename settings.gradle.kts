de43d3925106fafa625067e814a2dac8f4ec0b2a
ChananthonTha <koonzee14@gmail.com>
2022-10-18 17:42:58 +0700

    update

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "HELLO_KOTLIN1"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(){
+
+    kDataType1()
+    kDataType2()
+    kDataType3()
+    kDataType4()
+
+    println("# Any Type\n=========================================")
+    val kAny1: Any = 20
+    val kAny2: Any = 100.4
+    val kAny3: Any = "Mark Zuckerberg"
+    println("kAny1: $kAny1")
+    println("kAny2: $kAny2")
+    println("kAny3: $kAny3")
+
+//    var a = 10 ; var b = 5 ; var c = a+b
+//    println(c)
+//    println("Hello Kotlin")
+//    println("สวัสดีชาวโลก")
+//    println(5+5)
+//    println("วันนี้" + "วันอังคาร")
+//    print("เข้าสู่การเขียนโปรแกรม \n")
+//    println("\tสำหรับผู้มาใหม่")
+}
