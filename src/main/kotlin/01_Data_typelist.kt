5cd9c907632d8e0e89aa48f73b43a69265f03dfa
ChananthonTha <koonzee14@gmail.com>
2022-10-18 17:44:53 +0700

    update

--- src/main/kotlin/01_Data_typelist.kt
+++ src/main/kotlin/01_Data_typelist.kt
@@ -1,29 +1,29 @@
 fun kDataType1(){
-    println("# signed Intergral Type\n===================================")
-    val kByte: UByte = 255u
-    val kShort: UShort = 65_535u
+    println("# UnSigned Integral Type\n===============")
+    val kByte:UByte = 255u
+    val kShort:UShort = 65_535u
     println("kByte = $kByte")
     println("kShort = $kShort")
 }
 
 fun kDataType2(){
-    println("# signed Intergral Type\n===================================")
-    val kInt = 2_147_483_647
-    val kLong = 9_223_372_036_854_775_807L
+    println("# Signed Integral Type\n===============")
+    val kInt: Int = 2_147_483_647
+    val kLong: Long = 9_223_372_036_854_775_807L
     println(kInt)
     println(kLong)
 }
 
 fun kDataType3(){
-    println("# Boolean Type\n=========================================")
+    println("# Boolean Type\n===============")
     val kBoolean1: Boolean = true
     println("Mark = $kBoolean1")
 }
 
 fun kDataType4(){
-    println("# Floating point Type\n==================================")
+    println("# Floating point Type\n===============")
     val kFloat: Float = 3.14F
     val kDouble: Double = 1.79
-    println("kloating = $kFloat")
+    println("KFloating = $kFloat")
     println("kDouble = $kDouble")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,20 +5,19 @@ fun main(){
     kDataType3()
     kDataType4()
 
-    println("# Any Type\n=========================================")
+    println("# Any Type\n===============")
     val kAny1: Any = 20
     val kAny2: Any = 100.4
     val kAny3: Any = "Mark Zuckerberg"
     println("kAny1: $kAny1")
-    println("kAny2: $kAny2")
-    println("kAny3: $kAny3")
-
-//    var a = 10 ; var b = 5 ; var c = a+b
+    println("kAny2 = $kAny2")
+    println("kAny3 = $kAny3")
+//    val a = 10 ; val b = 5 ; val c = a+b
 //    println(c)
 //    println("Hello Kotlin")
 //    println("สวัสดีชาวโลก")
 //    println(5+5)
 //    println("วันนี้" + "วันอังคาร")
 //    print("เข้าสู่การเขียนโปรแกรม \n")
-//    println("\tสำหรับผู้มาใหม่")
+//    println("\t สำหรับผุ้มาใหม่")
 }
